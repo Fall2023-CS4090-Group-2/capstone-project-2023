5c350cbe1e006d8eba14f179a0a1b12cb0d7b90c
Adam <adamwmcneil@gmail.com>
2023-09-20 16:57:55 -0500

    hello world pygame project

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame, sys
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Hello World")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
