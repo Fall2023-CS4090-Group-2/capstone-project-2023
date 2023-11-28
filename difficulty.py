e67b8dc0cc1186a285248f9aeacda6736ee491a0
Payton Redemer <paytonredemer@gmail.com>
2023-11-28 12:22:26 -0600

    adjust difficulty

--- difficulty.py
+++ difficulty.py
@@ -10,21 +10,21 @@ class Difficulty(Enum):
 enemy_stats = {
     Difficulty.EASY: {
         "speed": 1,
-        "max_enemies": 3,
+        "max_enemies": 5,
         "damage": 5,
         "score": 5,
         "stop_condition": 5,
     },
     Difficulty.MEDIUM: {
         "speed": 2,
-        "max_enemies": 4,
+        "max_enemies": 8,
         "damage": 10,
         "score": 8,
         "stop_condition": 10,
     },
     Difficulty.HARD: {
         "speed": 3,
-        "max_enemies": 5,
+        "max_enemies": 10,
         "damage": 15,
         "score": 10,
         "stop_condition": 15,
